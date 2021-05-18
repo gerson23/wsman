fde89a7b40a23a7e9cb3184841fca5c74c59fbc1
Gerson Carlos <gerson.mtg@gmail.com>
2021-05-18 16:45:02 -0300

    Minimal working app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './classes/Main.js';
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    document.title = "Wsman"
+  })
   return (
     <Main></Main>
   );
--- src/classes/Main.js
+++ src/classes/Main.js
@@ -5,7 +5,7 @@ import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import { Divider, InputAdornment, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Divider, InputAdornment, ListItemIcon, ListItemText, Paper } from '@material-ui/core';
 import { ErrorOutline, Refresh, CheckCircle, CallMade, CallReceived } from '@material-ui/icons';
 import { green, red } from '@material-ui/core/colors';
 
@@ -36,25 +36,33 @@ class Main extends React.Component {
     }
 
     _handleConnect() {
-
-        this.setState({connection: 'connecting', error: false})
-        var socket = new WebSocket("ws://" + this.state.url)
-        socket.onopen = (event) => {
-            this.setState({connection: 'connected', error: false})
+        if (this.state.connection === 'connected') {
+            this.state.socket.close()
+            this.setState({connection: 'disconnected'})
         }
-        socket.onerror = (event) => {
-            this.setState({error: false, connection: 'connected'})
-            console.log(event)
-            console.log(this.state.socket)
+        else {
+            this.setState({connection: 'connecting', error: false})
+            var socket = new WebSocket("ws://" + this.state.url)
+            socket.onopen = (event) => {
+                this.setState({connection: 'connected', error: false})
+            }
+            socket.onerror = (event) => {
+                this.setState({error: true})
+            }
+            socket.onmessage = (event) => {
+                var history = this.state.history
+                history.push({direction: 'inbound', message: event.data, timestamp: Date()})
+                this.setState({history: history})
+            }
+            this.setState({socket: socket})
         }
-        this.setState({socket: socket})
     }
 
     _handleSendMessage() {
-        if (this.state.socket && this.state.socket.readyState != WebSocket.OPEN) {
-            //this.socket.send(this.message)
+        if (this.state.socket && this.state.socket.readyState === WebSocket.OPEN) {
+            this.state.socket.send(this.state.message)
             var history = this.state.history
-            history.push({direction: 'outboud', message: this.state.message})
+            history.push({direction: 'outboud', message: this.state.message, timestamp: Date()})
             this.setState({history: history})
         }
     }
@@ -65,28 +73,52 @@ class Main extends React.Component {
         if (this.state.connection === 'connected' && !this.state.error) return <CheckCircle style={{color: green['A700']}} />
     }
 
+    _getConnectButton() {
+        if (this.state.connection !== 'connected') {
+            return(
+                <Button variant="contained" color="primary" onClick={this._handleConnect}>Connect</Button>
+            )
+        }
+        else {
+            return(
+                <Button variant="contained" color="secondary" onClick={this._handleConnect}>Disconnect</Button>
+            )
+        }
+
+    }
+
     _renderListItem(item) {
-        return (
-            <ListItem button>
-                <ListItemIcon>
-                    {(() => {
-                        if(item.direction == 'outboud'){
-                            return(<CallMade style={{color: green['A400']}}/>)
-                        }
-                        else {
-                            return(<CallReceived style={{color: red['A400']}}/>)
-                        }
-                    })()}
+        if(item.direction == 'outboud'){
+            return (
+                <ListItem button>
+                    <ListItemIcon>
+                        <CallMade style={{color: green['A400']}}/>
                     </ListItemIcon>
-                <ListItemText>{item.message}</ListItemText>
-            </ListItem>
-        )
+                    <ListItemText
+                        primary={item.message}
+                        secondary={item.timestamp.toLocaleString()}
+                    />
+                </ListItem>
+            )
+        }
+        else {
+            return (
+                <ListItem button>
+                    <ListItemIcon>
+                        <CallReceived style={{color: red['A400']}}/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={item.message}
+                        secondary={item.timestamp.toLocaleString()}
+                    />
+                </ListItem>
+            )
+        }
     }
 
     render() {
         return (
             <div className="App" style={{margin: 10, display: 'flex', flexDirection: 'column'}}>
-                <h2>WebSocket Client</h2>
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
                     {this._checkStatus()}
                     <FormControl variant="outlined" style={{flexGrow: 1, paddingRight: 10, paddingLeft: 10}}>
@@ -96,29 +128,35 @@ class Main extends React.Component {
                         label="WebSocket URL"
                         variant="outlined"
                         onChange={this.setUrl}
-                        InputProps={{startAdornment: <InputAdornment position="start">ws://</InputAdornment>,}}
+                        InputProps={{startAdornment: <InputAdornment position="start">ws://</InputAdornment>, spellCheck: 'false'}}
                     />
                     </FormControl>
-                    <Button variant="contained" color="primary" onClick={this._handleConnect}>Connect</Button>
+                    {this._getConnectButton()}
                 </div>
                 <Divider style={{margin: 10}}/>
-                <div style={{display: 'flex'}}>
+                <div style={{display: 'flex', paddingBottom: 10}}>
                     <FormControl style={{flexGrow: 1}}>
                         <TextField
+                            disabled={this.state.connection != 'connected'}
                             id="message"
                             label="Message"
+                            helperText="i.e. new-messages/subscribe"
                             value={this.state.message}
                             onChange={this.setMessage}
+                            InputProps={{spellCheck: 'false'}}
                         />
                     </FormControl>
-                    <Button onClick={this._handleSendMessage}>Send to Socket</Button>
+                    <Button
+                        disabled={this.state.connection != 'connected'}
+                        onClick={this._handleSendMessage}
+                    >Send to Socket</Button>
                 </div>
                 <div>
-                    <List>
-                        {this.state.history.map((item) =>
-                            this._renderListItem(item)
-                        )}
-                    </List>
+                        <List style={{maxHeight: 400, overflow: 'auto'}}>
+                            {this.state.history.slice(0).reverse().map((item) =>
+                                this._renderListItem(item)
+                            )}
+                        </List>
                 </div>
             </div>
         );
